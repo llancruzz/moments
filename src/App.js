c8899e1450e895268cce1d221d49241077228b01
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-22 12:51:16 +0000

    feat: Added links to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hellow, World</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>}></Route>
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>}></Route>
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>}></Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
