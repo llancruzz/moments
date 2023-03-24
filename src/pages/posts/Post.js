ca6462d266f2584cef04962882dfa99429c85437
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-24 14:56:43 +0000

    feat: Created Comment Component so that the users can display the comments they created

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -7,6 +7,7 @@ import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { MoreDropdown } from "../../components/MoreDropdown";
 
+
 const Post = (props) => {
   const {
     id,
