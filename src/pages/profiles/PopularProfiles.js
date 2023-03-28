26a603f379ddd042b0651a3c4375ac8b415371bc
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-28 09:15:04 +0000

    feat: Create profile data context

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,45 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Asset from "../../components/Asset";
+import { useProfileData } from "../../contexts/ProfileDataContext";
+import Profile from "./Profile";
 
-const PopularProfiles = () => {
-  const [profileData, setProfileData] = useState({
-    // wel will use the pageProfile later!
-    pageProfile: { results: [] },
-    PopularProfiles: { results: [] },
-  });
-  const { PopularProfiles } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          PopularProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+const PopularProfiles = ({ mobile }) => {
+  const { popularProfiles } = useProfileData();
 
   return (
-    <Container className={appStyles.Content}>
-      {PopularProfiles.results.length ? (
+    <Container
+      className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
+      {popularProfiles.results.length ? (
         <>
           <p>Most followed profiles.</p>
-          {PopularProfiles.results.map((profile) => (
-            <p key={profile.id}>{profile.owner}</p>
-          ))}
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <Profile key={profile.id} profile={profile} mobile />
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <Profile key={profile.id} profile={profile} />
+            ))
+          )}
         </>
       ) : (
         <Asset spinner />
